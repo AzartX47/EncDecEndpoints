3df0aea637c030e6a3245914fce64ff8739f4999
Arseniy D. <arseniydubovkin@gmail.com>
2025-09-30 08:46:57 -0700

    adding sample code

---
+++ samples/animekai.py
@@ -0,0 +1,48 @@
+import requests
+
+# --- Config ---
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "Connection": "keep-alive"
+}
+
+API = "https://enc-dec.app/api"
+KAI_AJAX = "https://animekai.to/ajax"
+
+def encrypt(text):
+    return requests.get(f"{API}/enc-kai?text={text}").json()["result"]
+
+def decrypt(text):
+    return requests.post(f"{API}/dec-kai?text={text}").json()["result"]
+
+def parse_html(html):
+    return requests.post(f"{API}/parse-html", json={"text": html}).json()["result"]
+
+def get_json(url):
+    return requests.get(url, headers=HEADERS).json()
+
+# --- Cyberpunk Edgerunners ---
+# https://animekai.to/watch/cyberpunk-edgerunners-x6qm
+content_id = "c4G-8K8"
+
+# Episodes data
+enc_id = encrypt(content_id)
+episodes_resp = get_json(f"{KAI_AJAX}/episodes/list?ani_id={content_id}&_={enc_id}")
+episodes = parse_html(episodes_resp["result"])
+
+# Pick first episode token to load servers
+token = episodes["1"]["token"]
+enc_token = encrypt(token)
+servers_resp = get_json(f"{KAI_AJAX}/links/list?token={token}&_={enc_token}")
+servers = parse_html(servers_resp["result"])
+
+# Pick first server lid to load embed
+lid = servers["sub"]["1"]["lid"]
+enc_lid = encrypt(lid)
+embed_resp = get_json(f"{KAI_AJAX}/links/view?id={lid}&_={enc_lid}")
+encrypted = embed_resp["result"]
+
+# Decrypt
+decrypted = decrypt(encrypted)
+print(f"\n{'-'*25} Decrypted Data {'-'*25}\n")
+print(decrypted)
---
+++ samples/megaup.py
@@ -0,0 +1,23 @@
+import requests
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "Connection": "keep-alive"
+}
+
+API = "https://enc-dec.app/api"
+
+# --- Cyberpunk Edgerunners ---
+embed = "https://megaup.live/e/1sitL2f3WS2JcOLwGLtN7RfpCQ"
+
+# Replace /e/ with /media/
+media = embed.replace("/e/", "/media/")
+
+# Get encrypted media data
+encrypted = requests.get(media, headers=HEADERS).json()['result']
+
+# Decrypt
+decrypted = requests.post(f"{API}/dec-mega", json={"text": encrypted, "agent": HEADERS["User-Agent"]}).json()['result']
+
+print(f"\n{'-'*25} Decrypted Data {'-'*25}\n")
+print(decrypted)
