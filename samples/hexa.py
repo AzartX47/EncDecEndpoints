f94fe1c69216c1671708ce27da295b4a16ca4384
Arseniy D. <arseniydubovkin@gmail.com>
2025-11-04 21:37:42 -0800

    hexa/flixer: update for latest

--- samples/hexa.py
+++ samples/hexa.py
@@ -1,9 +1,13 @@
 import requests
 from Crypto.Random import get_random_bytes
 
+# Generate 32-byte hex key
+key = get_random_bytes(32).hex()
+
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
-    "Accept": "plain/text"
+    "Accept": "plain/text",
+    "X-Api-Key": key,
 }
 
 API = "https://enc-dec.app/api"
@@ -20,10 +24,6 @@ imdb_id = "tt12590266"
 tmdb_id = "105248"
 season = "1"
 episode = "1"
-
-# Generate 32-byte hex key
-key = get_random_bytes(32).hex()
-HEADERS["X-Api-Key"] = key
 
 # Get encrypted text
 url = f"https://themoviedb.hexa.watch/api/tmdb/tv/{tmdb_id}/season/{season}/episode/{episode}/images"
